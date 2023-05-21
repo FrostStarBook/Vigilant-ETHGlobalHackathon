8d14fe33e2c2ec4bfdfae6c1c1e0bd53484c2969
Yiyang Li <yiyangl@google.com>
2023-05-20 21:29:09 -0700

    Create a rectangle when starting the game

---
+++ packages/client/src/game/Game.ts
@@ -0,0 +1,25 @@
+import { GameBase } from "./Game.generated";
+const { regClass, property } = Laya;
+
+@regClass()
+export class Game extends GameBase {
+    //declare owner : Laya.Sprite3D;
+    chunkSideLength: number = 64;     
+    
+    fromChunk: Rectangle;
+    nextChunk:Rectangle;
+    bgRes:Laya.Texture;
+    speed:number = 500;
+    onAwake() {
+        console.log("Game start");
+        const bottomLeftX = 0;
+        const bottomLeftY = 0;
+        const bottomLeft = { x: bottomLeftX, y: bottomLeftY };
+
+        this.fromChunk = {
+            bottomLeft,
+            sideLength: this.chunkSideLength,
+            };
+        this.nextChunk = this.fromChunk;  
+    }
+}

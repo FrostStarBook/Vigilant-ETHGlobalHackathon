f33cab4a980ca9b4ddd1bd25ac082aa8a91a7bda
Jiequan Zhang <jiequanzhang@Jiequans-MBP.attlocal.net>
2023-05-20 17:27:03 -0700

    update components for client

--- packages/client/src/mud/contractComponents.ts
+++ packages/client/src/mud/contractComponents.ts
@@ -5,12 +5,80 @@ import { defineComponent, Type as RecsType, World } from "@latticexyz/recs";
 
 export function defineContractComponents(world: World) {
   return {
-    Counter: (() => {
-      const tableId = new TableId("", "Counter");
+    ObstructionComponent: (() => {
+      const tableId = new TableId("", "ObstructionCompo");
       return defineComponent(
         world,
         {
-          value: RecsType.Number,
+          value: RecsType.Boolean,
+        },
+        {
+          metadata: {
+            contractId: tableId.toHexString(),
+            tableId: tableId.toString(),
+          },
+        }
+      );
+    })(),
+    PlayerComponent: (() => {
+      const tableId = new TableId("", "PlayerComponent");
+      return defineComponent(
+        world,
+        {
+          value: RecsType.Boolean,
+        },
+        {
+          metadata: {
+            contractId: tableId.toHexString(),
+            tableId: tableId.toString(),
+          },
+        }
+      );
+    })(),
+    PositionComponent: (() => {
+      const tableId = new TableId("", "PositionComponen");
+      return defineComponent(
+        world,
+        {
+          x: RecsType.BigInt,
+          y: RecsType.BigInt,
+        },
+        {
+          metadata: {
+            contractId: tableId.toHexString(),
+            tableId: tableId.toString(),
+          },
+        }
+      );
+    })(),
+    PlayerInfoComponent: (() => {
+      const tableId = new TableId("", "PlayerInfoCompon");
+      return defineComponent(
+        world,
+        {
+          updateTimestamp: RecsType.BigInt,
+          state: RecsType.Number,
+          energy: RecsType.Number,
+        },
+        {
+          metadata: {
+            contractId: tableId.toHexString(),
+            tableId: tableId.toString(),
+          },
+        }
+      );
+    })(),
+    MapComponent: (() => {
+      const tableId = new TableId("", "MapComponent");
+      return defineComponent(
+        world,
+        {
+          width: RecsType.BigInt,
+          height: RecsType.BigInt,
+          seed: RecsType.BigInt,
+          denom: RecsType.BigInt,
+          precision: RecsType.BigInt,
+          stepLimit: RecsType.BigInt,
         },
         {
           metadata: {
